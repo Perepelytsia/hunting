c9984b6cc9aff7192213c0fb9433c31ac6b3b4a9
Perepelytsia <perepelytsa@devoler.com>
2022-07-26 06:47:48 +0300

    change test

--- internal/symbols/symbols_test.go
+++ internal/symbols/symbols_test.go
@@ -2,9 +2,22 @@ package symbols
 
 import "testing"
 
-func TestGetSymbols(t *testing.T) {
+func TestGetSymbolsLen(t *testing.T) {
 	symbols := GetSymbols()
 	if len(symbols) != 16 {
 		t.Error("result should be 16 symbols, got", len(symbols))
+	}
+}
+
+func TestGetSymbolsWall(t *testing.T) {
+	fail := true
+	for _, symbol := range GetSymbols() {
+		if symbol == '#' {
+			fail = false
+			break
+		}
+	}
+	if fail {
+		t.Error("there is no #, got emptyness")
 	}
 }
