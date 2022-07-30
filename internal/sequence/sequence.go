3f8baa35093ff1a4acab76ff29bba45e9f56c8d6
Perepelytsia <perepelytsa@devoler.com>
2022-07-30 11:39:16 +0300

    add optimization 1

--- internal/sequence/sequence.go
+++ internal/sequence/sequence.go
@@ -189,14 +189,18 @@ func PrintMemUsage() {
 	var m runtime.MemStats
 	runtime.ReadMemStats(&m)
 	// For info on each, see: https://golang.org/pkg/runtime/#MemStats
-	fmt.Printf("Alloc = %v GiB", bToGb(m.Alloc))
+	fmt.Printf("Alloc = %v MiB", bToMb(m.Alloc))
 	fmt.Printf("\tTotalAlloc = %v GiB", bToGb(m.TotalAlloc))
-	fmt.Printf("\tSys = %v GiB", bToGb(m.Sys))
+	fmt.Printf("\tSys = %v MiB", bToMb(m.Sys))
 	fmt.Printf("\tNumGC = %v\n", m.NumGC)
 }
 
 func bToGb(b uint64) uint64 {
 	return b / 1024 / 1024 / 1024
+}
+
+func bToMb(b uint64) uint64 {
+	return b / 1024 / 1024
 }
 
 func GetResult() int {
